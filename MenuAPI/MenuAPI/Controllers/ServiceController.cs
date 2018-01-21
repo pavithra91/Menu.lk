769d3c52d3d38703a322decf5bc43a9139650fc7
Pavithra <pavidsscst@gmail.com>
2018-01-21 13:41:23 +0530

    First Commit

---
+++ MenuAPI/MenuAPI/Controllers/ServiceController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Http;
+using System.Linq;
+
+namespace MenuAPI.Controllers
+{
+    public class ServiceController : ApiController
+    {
+        SystemEntities _context = new SystemEntities();
+        
+        // GET: Service
+        [HttpGet]
+        public dynamic Get()
+        {
+            var json = _context.Restaurants.Where(w=>w.IsActive == true).Select( i => new { i.R_Name, i.R_Image, i.R_OpenTime, i.R_DeliveryTime, i.R_Rating, i.R_Tags });
+            return json;
+        }
+    }
+}
